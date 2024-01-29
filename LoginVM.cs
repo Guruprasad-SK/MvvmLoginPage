1fb35c224bb25d0f8be9f747d031e5c62078fd4b
Guruprasad S K <115786145+Guruprasad-SK@users.noreply.github.com>
2024-01-29 14:38:02 +0530

    Add files via upload

---
+++ LoginVM.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DemoMvvm.Models;
+using System.ComponentModel;
+using System.Windows.Input;
+using DemoMvvm.Commands;
+using System.Windows;
+
+namespace DemoMvvm.ViewModels
+{
+    public class LoginVM: ViewModelBase
+    {
+        private User user;
+       private string userName;
+        public ICommand LoginCommand { get; }
+
+        public LoginVM()
+        {
+            user = new User();
+            LoginCommand = new RelayCommands((param) => LoggedIn(param));
+        }
+        public string UserName
+        {
+            get => user.UserName;
+            set
+            {
+                user.UserName = value;
+                OnPropertyChanged(nameof(UserName));
+            }
+        }
+        public string PassWord
+        {
+            get => user.PassWord;
+            set
+            {
+                user.PassWord = value;
+                OnPropertyChanged(nameof(PassWord));
+
+            }
+        }
+        private void LoggedIn(object param)
+        {
+            MessageBox.Show($"Loged in Succefull as {param}");
+        }
+
+
+
+    }
+}
---
+++ RelayCommands.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace DemoMvvm.Commands
+{
+    public class RelayCommands : ICommand
+    {
+        private readonly Action<object> _executeAction;
+        public RelayCommands(Action<object> executionAction)
+        {
+            _executeAction = executionAction;
+        }
+        public bool CanExecute(object parameter)=> true;
+        public void Execute(object parameter) => _executeAction(parameter);
+        public event EventHandler CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested += value; }
+        }
+    }
+}
